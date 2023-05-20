bafb44077c5ce96245821e62c4bae07e9fe44e9e
ZalakG <jhalakgajjar@hotmail.com>
2023-05-20 08:00:07 -0400

    added code to file

--- SurfUp/app.py
+++ SurfUp/app.py
@@ -1,5 +1,12 @@
 # Import the dependencies.
-
+from flask import Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import numpy as np
+import pandas as pd
+import datetime as dt
 
 
 #################################################
@@ -8,23 +15,38 @@
 
 
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
 
 # reflect the tables
+Base.prepare(engine, reflect = True)
 
 
 # Save references to each table
-
+Measurement  = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
-
+session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def default ():
+    routes = (
+        f'<a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a><br>'
+        f'/api/v1.0/stations<br>'
+    )
+    return routes
+
+
+if __name__ == '__main__':
+    app.run()
